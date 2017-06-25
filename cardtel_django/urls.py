c229a895c51b8ca2361b02fba80f1c85c16587f3
Rachel Marincola <rachel@doordash.com>
2017-06-25 16:15:13 -0700

    add media url back in.

--- cardtel_django/urls.py
+++ cardtel_django/urls.py
@@ -12,6 +12,6 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', index),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
