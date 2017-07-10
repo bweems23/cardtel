af4dee3c17e4485b78258693589e9f59fbba96ca
Ben Weems <bweems@stanford.edu>
2017-07-09 18:06:53 -0700

    Added comments to all functions.

--- cardtel/utils/playing.py
+++ cardtel/utils/playing.py
@@ -1,25 +1,64 @@
+import deuces
+import gameplay
+from django.db import transaction
+
+def play_cards(player, cards):
+    if check_valid_play(player, cards):
+        for card in cards:
+            play_card(player, card)
+    else:
+        raise IllegalPlay
+
+@transaction.atomic
 def play_card(player, card):
-    raise NotImplementedError
+    player.cards.remove(card)
+    player.game.table.add(card)
 
 def fold(player):
-    raise NotImplementedError
+    player.has_folded = True
+    player.save()
 
-def check_valid_play(player, card):
-    raise NotImplementedError
+def check_valid_play(player, new_cards):
+    table_deuces_cards = get_deuces_cards(player.game.table.cards.all())
+    new_deuces_cards = get_deuces_cards(new_cards)
+    all_cards = new_deuces_cards + table_deuces_cards
+    current_score = player.game.table.best_hand_score
+    updated_score = evaluate_cards(all_cards)
 
-def update_best_hand(table):
-    # This will be stored on the Table
-    raise NotImplementedError
+    # Check that every card makes the hand better
+    for index in xrange(len(new_deuces_cards)):
+        tmp_cards = all_cards[:index] + all_cards[index+1:]
+        tmp_score = evaluate_cards(tmp_cards)
+        if tmp_score <= updated_score:
+            return False
 
-def finish_game(game):
-    raise NotImplementedError
+    # Check that the score has been improved
+    if updated_score < current_score:
+        table.best_hand_score = updated_score
+        table.save()
+        return True
+    else:
+        return False
 
 def finish_hand(game):
     raise NotImplementedError
 
 def finish_point(game):
-    # We'll want to call clear_table here
+    winner = game.last_to_play_card
+    winner.score = winner.score + 1
+    if game.is_over():
+        gameplay.finish_game(game)
+    elif game.hand_is_over():
+        finish_hand(game)
+    game.players.update(has_folded=False)
+    winner.save()
+
+def evaluate_cards(cards):
     raise NotImplementedError
 
-def clear_table(table):
-    raise NotImplementedError
+def get_deuces_cards(cards):
+    return [deuces.Card.new(card.number + card.suit)
+            for card in player.game.table.cards.all()]
+
+class IllegalPlay(Exception):
+    '''Raise when a user attempts a play that is not legal.'''
