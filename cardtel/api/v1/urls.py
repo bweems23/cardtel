9f7644a503b9b0ead15bc362db10a74ec4de994d
Rachel Marincola <rachel@doordash.com>
2017-08-13 12:18:53 -0700

    cleanup.

--- cardtel/api/v1/urls.py
+++ cardtel/api/v1/urls.py
@@ -2,13 +2,7 @@ from rest_framework import routers
 from django.conf.urls import include, url
 from cardtel.api.v1 import api
 
-# router = routers.DefaultRouter()
-# router.register(r'games', api.GameListView.as_view({'get': 'list'}), base_name='get')
-
-# urlpatterns = [
-#     url(r'^games/$', include(router.urls)),
-# ]
 
 urlpatterns = [
     url(r'^games/$', api.GameListView.as_view()),
-]
+]s
